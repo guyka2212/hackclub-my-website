192e515717cabac058f309f5e17b2f72f84a872c
Guy Kadosh <guykadosh22@gmail.com>
2026-09-07 10:46:07 +0300

    Update website

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+function showphoto() {
+    const photo = document.getElementById("kimi-and-max");
+    const textkimiandmax = document.getElementById("textkimiandmax");
+    photo.style.display = photo.style.display === "none" ? "block" : "none";
+    textkimiandmax.style.display = textkimiandmax.style.display === "none" ? "block" : "none";
+}
+
 function themeChange() {
 
     const isDark = document.body.classList.toggle("dark-theme");
-    
+
     document.getElementById("text").style.color = isDark ? "white" : "black";
 }
